c488a956a217f7bd5a3d1bf0491f8697798c36bb
SpookyEngineer <gabrielicmusa@gmail.com>
2023-06-06 15:38:40 -0300

    Refactoring and formatting project

--- Src/music.js
+++ Src/music.js
@@ -1,21 +1,22 @@
-const button = document.getElementById('play');
-const music = new Audio('music.3gp');
+const button = document.getElementById("play");
+const music = new Audio("music.3gp");
 
-button.addEventListener('click', function(){ //Toggles music and button
-    switch(button.value){
-        case "Play":
-            music.play()
-            music.loop = true;
-            music.volume = 0.5;
-                button.value = "Stop";
-                button.alt = "Stop"
-                button.src = "Images/play.png"
-             break;
-        case "Stop":
-            music.pause()
-                button.value = "Play";
-                button.alt = "Play"
-                button.src = "Images/pause.png"
-             break;
-   }
-})
+button.addEventListener("click", function () {
+  //Toggles music and button
+  switch (button.value) {
+    case "Play":
+      music.play();
+      music.loop = true;
+      music.volume = 0.5;
+      button.value = "Stop";
+      button.alt = "Stop";
+      button.src = "../Images/play.png";
+      break;
+    case "Stop":
+      music.pause();
+      button.value = "Play";
+      button.alt = "Play";
+      button.src = "../Images/pause.png";
+      break;
+  }
+});
